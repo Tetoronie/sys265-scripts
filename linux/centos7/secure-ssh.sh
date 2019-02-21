83eeb6a8c74cc92a902244a47bf59a25b4ec4bf4
Tetoronie <chrismathieson25@gmail.com>
2019-02-21 14:59:27 -0500

    Modified SSH script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -5,3 +5,16 @@
 # Removes roots ability to ssh in
 
 echo "testing 123"
+
+useradd $1
+echo User $1 added
+
+mkdir -p /home/$1/.ssh/authorized_keys
+
+cp /home/chris/sys265-scripts/linux/public-keys/sys265.pub /home/$1/.ssh/authorized_keys/
+
+chmod 700 /home/$1/.ssh/
+
+chmod 600 /home/$1/.ssh/authorized_keys
+
+chown -R $1:$1 /home/$1/.ssh
